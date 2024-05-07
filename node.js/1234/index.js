7df2b53f5bc750e44e41f3053b1aed6e24fb3df1
JackSHansen <142877764+JackSHansen@users.noreply.github.com>
2024-05-07 08:41:29 +0200

    Update index.js

--- node.js/1234/index.js
+++ node.js/1234/index.js
@@ -2,3 +2,9 @@ import http from 'http';
 http.createServer((request, response) => {
     console.log('Hello world');
 }).listen(4000)
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Hello World!');
+    response.end();
+}).listen(4000)
